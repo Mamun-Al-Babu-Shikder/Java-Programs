02684214c6124df41f8a74f214134bcbe5ea7625
Mamun-Al-Babu-Shikder <49284765+Mamun-Al-Babu-Shikder@users.noreply.github.com>
2019-10-03 22:09:28 +0600

    Upload SortByGPA class

---
+++ Basic/Object Sorting/SortByGPA.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class SortByGPA implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1.getCgpa() < o2.getCgpa()) {
+            return 1;
+        } else if (o1.getCgpa() > o2.getCgpa()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
